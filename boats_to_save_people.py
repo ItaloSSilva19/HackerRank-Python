b316ecef1c57a61d14d6cb9fab40fd53a725c472
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-09-21 18:09:27 -0300

    Create boats_to_save_people.py

---
+++ boats_to_save_people.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        minimum_boat_number = 0
+        a = sorted(people, reverse=True)
+        while a:
+            if a[0] + a[-1] <= limit and len(a) > 1:
+                minimum_boat_number += 1
+                del(a[0])
+                del(a[-1])
+            else:
+                print('b')
+                minimum_boat_number += 1
+                del(a[0])
+
+            
+        return minimum_boat_number
