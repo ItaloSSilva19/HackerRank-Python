934f6f0b5f33d60573b3d6e60d4950aea2c2e8e7
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-05 15:26:19 -0300

    Update-rangoli_aphabet-Alphabet done
    Create the other half of the alphabet

--- rangoli_alphabet.py
+++ rangoli_alphabet.py
@@ -11,8 +11,12 @@ def print_rangoli(n):
         else:
             sequence = alphabet[n-1:i:-1] + alphabet[i] + alphabet[i+1:n]
         rangoli.append(sequence)
-
-            
+    
+    #Mirror the first half to generate the alphabet
+    mirror_rangoli = rangoli.copy()
+    mirror_rangoli.pop()
+    rangoli.extend(reversed(mirror_rangoli))
+    
  
 if __name__ == '__main__':
     n = int(input())
