0e442bafacf225306a6f53a69f430ea56b7f185c
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-05 15:25:05 -0300

    Update-rangoli_alphabet
    Create a for loop that creates the first half of the alphabet rangoli as a list

--- rangoli_alphabet.py
+++ rangoli_alphabet.py
@@ -1,6 +1,18 @@
 
 def print_rangoli(n):
- 
+    import string
+    alphabet = string.ascii_lowercase
+    rangoli = []
+    
+    #Create half of the rangoli alphabet
+    for i in range(n-1,-1,-1):
+        if i == n-1:
+            sequence = alphabet[i]
+        else:
+            sequence = alphabet[n-1:i:-1] + alphabet[i] + alphabet[i+1:n]
+        rangoli.append(sequence)
+
+            
  
 if __name__ == '__main__':
     n = int(input())
