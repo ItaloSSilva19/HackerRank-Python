0305bdd210044ec9360bf894b5afdcd53c43a13f
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-06 22:57:27 -0300

    Finish minion-game
    Add the variable to count points;
    Place the length of the string to a variable, because it is needed to decrease this value;
    Loop through the string to check if its starts with a vowel or not and count the points;
    Print the result

--- minion_game.py
+++ minion_game.py
@@ -1,4 +1,20 @@
 def minion_game(string):
+    vowel = 'AEIOU'
+    kevin = 0
+    stuart = 0
+    length_string = len(string)
+    for i in range(length_string):
+        if string[i] in vowel:
+            kevin += length_string
+        else:
+            stuart += length_string
+        length_string -= 1
+    if kevin == stuart:
+        print('Draw')
+    elif kevin > stuart:
+        print("Kevin", kevin)
+    else:
+        print("Stuart", stuart)
 
 if __name__ == '__main__':
     s = input()
