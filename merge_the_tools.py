48e9d975f70ad53bd5e74e9e5d3e630c8149a9f8
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-07 13:44:19 -0300

    Update merge_the_tools.py

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -1,1 +1,11 @@
-
+def merge_the_tools(string, k):
+    # your code goes here
+    length = len(string)
+    split_length = int(length/k)
+    string_list = []
+    for i in range(split_length):
+        temp = string[i * k : (i + 1) * k]
+        string_list.append(temp)
+    for i in string_list:
+        x = "".join(dict.fromkeys(i))
+        print(x)
